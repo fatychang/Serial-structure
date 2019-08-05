5a5886e7f5b6fadb68f71a2d07cfc72bb4f060cf
JSChang <papachang12@gmail.com>
2019-08-05 13:08:46 +0800

    Update serialKinematics.py
    Three joints chain kinematics calculation and visualization

--- serialKinematics.py
+++ serialKinematics.py
@@ -55,7 +55,7 @@ theta[1] = theta[1] - theta[0]
 # Transformation matrix from frame i to  frame (i-1), stores in list A
 A = list()
 num_joints = 2
-for i in range(2):
+for i in range(num_joints):
     T = np.matrix([[np.cos(theta[i]), -np.sin(theta[i])*np.cos(alpha[i]), np.sin(alpha[i])*np.sin(alpha[i]), a[i] *np.cos(theta[i])],
                 [np.sin(theta[i]), np.cos(theta[i])*np.cos(alpha[i]), -np.cos(theta[i])*np.sin(alpha[i]), a[i]*np.sin(theta[i])],
                 [0, np.sin(alpha[i]), np.cos(alpha[i]), d[i]],
@@ -75,6 +75,50 @@ y = np.array([p1.item(1), p2.item(1), ef.item(1)])
 plt.plot(x, y, 'o') #plot the joint and end-effector
 plt.plot(x, y,'r') #plot the link
 plt.title('Two joints chain kinematic visualization')
+
+
+##### two joints link chain
+# D-H parameters 
+a = np.array([1,1,1])
+d = np.array([0,0,0])
+alpha = np.array([0,0,0])
+theta = np.array([np.pi/4, np.pi/3, np.pi/4])
+
+# Modify the rotation angles
+theta[2] = theta[2] - theta[1]
+theta[1] = theta[1] - theta[0]
+
+# Transformation matrix from frame i to  frame (i-1), stores in list A
+A = list()
+num_joints = 3
+for i in range(num_joints):
+    T = np.matrix([[np.cos(theta[i]), -np.sin(theta[i])*np.cos(alpha[i]), np.sin(alpha[i])*np.sin(alpha[i]), a[i] *np.cos(theta[i])],
+                [np.sin(theta[i]), np.cos(theta[i])*np.cos(alpha[i]), -np.cos(theta[i])*np.sin(alpha[i]), a[i]*np.sin(theta[i])],
+                [0, np.sin(alpha[i]), np.cos(alpha[i]), d[i]],
+                [0, 0, 0, 1]])
+    A.append(T)
+
+# Calculate the end-effector position
+p1 = np.matrix([0, 0, 0, 1])
+p1 = p1.transpose()
+p2 = A[0]* p1
+p3 = A[0] *A[1] *p1
+ef = A[0] *A[1]* A[2] *p1
     
-    
-    
+# Visualizae the result
+plt.figure()
+x = np.array([p1.item(0), p2.item(0), p3.item(0), ef.item(0)])
+y = np.array([p1.item(1), p2.item(1), p3.item(1), ef.item(1)])
+plt.plot(x, y, 'o') #plot the joint and end-effector
+plt.plot(x, y,'r') #plot the link
+plt.title('Three joints chain kinematic visualization')
+
+
+
+
+
+
+
+
+
+
