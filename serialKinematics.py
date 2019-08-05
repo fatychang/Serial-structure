bc0df0beb61de955ee1a7644a3edce553b6e7a35
JSChang <papachang12@gmail.com>
2019-08-05 13:03:08 +0800

    Update serialKinematics.py
    Two joints chain kinematics calculation and visuailzation

--- serialKinematics.py
+++ serialKinematics.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 
 
-##### One link chain (one joint, i=1)
+##### One joint chain
 # D-H parameters
 a_1 = 1
 d_1 = 0
@@ -30,11 +30,51 @@ A1 = np.matrix([[np.cos(theta_1), -np.sin(theta_1)*np.cos(alpha_1), np.sin(alpha
 # Calculate the end-effector position
 p1 = np.matrix([0, 0, 0, 1])
 p1 = p1.transpose()
-p2 = A1*p1
+ef = A1*p1
 
 
 # Visualizae the result
-x = np.array([p1.item(0), p2.item(0)])
-y = np.array([p1.item(1), p2.item(1)])
+x = np.array([p1.item(0), ef.item(0)])
+y = np.array([p1.item(1), ef.item(1)])
 plt.plot(x, y, 'o') #plot the joint and end-effector
 plt.plot(x, y,'r') #plot the link
+plt.title('One joint chain kinematic visualization')
+
+
+
+##### two joints link chain
+# D-H parameters
+a = np.array([1,1])
+d = np.array([0,0])
+alpha = np.array([0,0])
+theta = np.array([np.pi/4, np.pi/3])
+
+# Modify the rotation angles
+theta[1] = theta[1] - theta[0]
+
+# Transformation matrix from frame i to  frame (i-1), stores in list A
+A = list()
+num_joints = 2
+for i in range(2):
+    T = np.matrix([[np.cos(theta[i]), -np.sin(theta[i])*np.cos(alpha[i]), np.sin(alpha[i])*np.sin(alpha[i]), a[i] *np.cos(theta[i])],
+                [np.sin(theta[i]), np.cos(theta[i])*np.cos(alpha[i]), -np.cos(theta[i])*np.sin(alpha[i]), a[i]*np.sin(theta[i])],
+                [0, np.sin(alpha[i]), np.cos(alpha[i]), d[i]],
+                [0, 0, 0, 1]])
+    A.append(T)
+
+# Calculate the end-effector position
+p1 = np.matrix([0, 0, 0, 1])
+p1 = p1.transpose()
+p2 = A[0]* p1
+ef = A[0] *A[1] *p1
+    
+# Visualizae the result
+plt.figure()
+x = np.array([p1.item(0), p2.item(0), ef.item(0)])
+y = np.array([p1.item(1), p2.item(1), ef.item(1)])
+plt.plot(x, y, 'o') #plot the joint and end-effector
+plt.plot(x, y,'r') #plot the link
+plt.title('Two joints chain kinematic visualization')
+    
+    
+    
