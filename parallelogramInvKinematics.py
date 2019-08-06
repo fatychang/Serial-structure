62ed91ae503ab982a6ae0e23b7a7935aef666b51
JSChang <papachang12@gmail.com>
2019-08-06 18:06:08 +0800

    Create parallelogramInvKinematics.py

---
+++ parallelogramInvKinematics.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  6 17:56:00 2019
+
+@author: jschang
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+########################
+##  INV KINEMATIC     ##
+########################
+
+# Known end-effector position
+ef = np.array([1,3.4641])
+
+
+# Design params
+length = np.array([1,2,1])
+
+
+# Calculate the position of the joint 3rd
+
+
+
+
+
+
+
